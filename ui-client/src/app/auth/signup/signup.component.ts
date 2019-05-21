c36325bfd9c054516182a0ef151cb3807c2f893b
Thomas Hoffman <purduetom90@gmail.com>
2019-05-21 11:50:45 -0500

    removed Bootstrap

--- ui-client/src/app/auth/signup/signup.component.ts
+++ ui-client/src/app/auth/signup/signup.component.ts
@@ -22,13 +22,46 @@ export class SignupComponent implements OnInit {
          email: new FormControl(null,
             { validators: [Validators.required, Validators.email] }),
          password: new FormControl(null,
-            { validators: [Validators.required] }),
+            { validators: [Validators.required, this.passwordValidator] }),
          company: new FormControl(null,
             { validators: [Validators.required, Validators.minLength(3)] }),
          agreeTermsConditions: new FormControl(null,
             { validators: [Validators.required] })
       });
 
+   }
+
+   // Password Complexity Rules:
+   // must be at least 8 characters long 
+   // must not contain spaces
+   // must contain at least one upper case letter
+   // must contain at least one number
+   // must contain one non-alphanumberic character.
+   passwordValidator(control: FormControl): { [key: string]: any } {
+      const value = control.value || '';
+      console.log("Validating password! " + value);
+      return null;
+
+      if (value.length < 8) {
+         console.warn("Failed Length Check");
+         return { password: { description: 'Password is too short' } };
+      }
+      if (value.match(/[ ]/)) {
+         console.warn("Failed space Check");
+         return { password: { description: 'Password must not contain spaces' } };
+      }
+      if (!value.match(/[A-Z]/)) {
+         console.warn("Failed UpperCaseChar Check");
+         return { password: { description: 'Password must contain one uppercase character' } };
+      }
+      if (!value.match(/[\d]/)) {
+         console.warn("Failed numberic char check");
+         return { password: { description: 'Password must contain one number' } };
+      }
+
+      // if (!value.match(/[^a-z0-9]/))
+      //    return { password: { description: 'Password must contain one non alphanumberic character' } };
+      return null;
    }
 
    formSubmit() {
